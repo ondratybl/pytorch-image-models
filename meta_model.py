8346dcb30396696cebc3e3df3b18436a035c5d23
ondratybl <73835066+ondratybl@users.noreply.github.com>
2024-02-07 10:56:18 +0100

    - there is only one model logit now (the other one being complement its negative value) - logits from models (i.e. features) standardized to sum to zero

--- meta_model.py
+++ meta_model.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import torch
-from torch.nn.functional import softmax
 
 
 class MetaModel(torch.nn.Module):
     def __init__(self, model1, model2):
         super().__init__()
-        self.weights = torch.nn.Parameter(torch.rand(1, 2))
+        self.logit = torch.nn.Parameter(torch.rand(1, 1))
         self.model1 = model1
         self.model2 = model2
         assert model1.num_classes == model2.num_classes, 'Incompatible models due to num_classes missmatch'
@@ -21,10 +20,20 @@ class MetaModel(torch.nn.Module):
         print(f'Number of trainable parameters for the meta model: {trainable_param_count}')
 
     def forward(self, x):
-        weights = softmax(self.weights, dim=-1)
-        y = torch.add(torch.mul(self.model1(x), weights[0, 0]), torch.mul(self.model2(x), weights[0, 1]))
-        output = softmax(y, dim=-1)
-        return output
+
+        # Weights for the two models
+        weights = torch.nn.functional.softmax(torch.cat([self.logit, -self.logit], dim=1), dim=-1)
+
+        # Standardized logits of the two models
+        model1_logits = self.model1(x)
+        model2_logits = self.model2(x)
+        model1_logits_std = model1_logits - torch.mean(model1_logits, dim=1, keepdim=True).repeat(1, self.num_classes)
+        model2_logits_std = model2_logits - torch.mean(model2_logits, dim=1, keepdim=True).repeat(1, self.num_classes)
+
+        # Combined output
+        y = torch.add(torch.mul(model1_logits_std, weights[0, 0]), torch.mul(model2_logits_std, weights[0, 1]))
+
+        return torch.nn.functional.softmax(y, dim=-1)
 
     def string(self):
-        return f'Weight for the first model: {softmax(self.weights, dim=-1)[0][0].item()}'
+        return f'Weight for the first model: {torch.nn.functional.softmax(torch.cat([self.logit, -self.logit], dim=1), dim=-1)[0][0].item()}'
