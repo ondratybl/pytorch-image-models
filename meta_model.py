fixed: softmax after combining models outputs; added: temperature can be set to custom value (and not optimized)

meta_model.py
@@ -5,11 +5,12 @@ from temperature_scaling import ModelWithTemperature
 
 
 class MetaModel(torch.nn.Module):
-    def __init__(self, model1, model2, init_logit=0.0):
+    def __init__(self, model1, model2, temperature, init_logit=0.0):
         super().__init__()
         self.logit = torch.nn.Parameter(torch.tensor([[init_logit]]))
-        self.model1 = ModelWithTemperature(model1) # created model has trainable temperature
-        self.model2 = ModelWithTemperature(model2)
+        self.model1 = ModelWithTemperature(model1, temperature)  # created model has trainable temperature
+        self.model2 = ModelWithTemperature(model2, temperature)
+        self.freeze_temperatures()
         assert model1.num_classes == model2.num_classes, 'Incompatible models due to num_classes missmatch'
         self.num_classes = model1.num_classes
 
@@ -25,13 +26,9 @@ class MetaModel(torch.nn.Module):
         model2_logits_std = model2_logits - torch.mean(model2_logits, dim=1, keepdim=True).repeat(1, self.num_classes)
 
         # Combined output
-        #y = torch.add(torch.mul(model1_logits_std, weights[0, 0]), torch.mul(model2_logits_std, weights[0, 1]))
+        y = torch.add(torch.mul(model1_logits_std, weights[0, 0]), torch.mul(model2_logits_std, weights[0, 1]))
 
-        #return torch.nn.functional.softmax(y, dim=-1)
-
-        model1_output = torch.nn.functional.softmax(model1_logits_std, dim=-1)
-        model2_output = torch.nn.functional.softmax(model2_logits_std, dim=-1)
-        return torch.add(torch.mul(model1_output, weights[0, 0]), torch.mul(model2_output, weights[0, 1]))
+        return torch.nn.functional.softmax(y, dim=-1)
 
     def string(self):
         return f'Logit for the first model: {self.logit[0].item()}, number of trainable params: {self.get_trainable_param_count()}.'
@@ -45,15 +42,25 @@ class MetaModel(torch.nn.Module):
 
     def set_and_freeze_temperatures(self, valid_loader):
 
+        # Unfreeze temperatures
+        for param_name, param in self.named_parameters():
+            if 'temperature' in param_name:
+                param.requires_grad = True
+
         # Optimize temperatures
         self.model1.set_temperature(valid_loader)
         self.model2.set_temperature(valid_loader)
 
         # Freeze temperatures
+        self.freeze_temperatures()
+
+    def freeze_temperatures(self):
+        # Freeze temperatures
         for param_name, param in self.named_parameters():
             if 'temperature' in param_name:
                 param.requires_grad = False
 
-        print(f'Temperature for Model1 set to: {self.model1.temperature[0].item()}')
-        print(f'Temperature for Model2 set to: {self.model2.temperature[0].item()}')
+        print(f'Temperature for Model1: {self.model1.temperature[0].item()}')
+        print(f'Temperature for Model2: {self.model2.temperature[0].item()}')
+
         print(self.string())

my_train.py
@@ -116,6 +116,8 @@ group.add_argument('--model1', default='resnet50', type=str, metavar='MODEL1',
                    help='Name of model 1 to be compared (default: "resnet50")')
 group.add_argument('--model2', default='resnet50', type=str, metavar='MODEL2',
                    help='Name of model 2 to be compared (default: "resnet50")')
+group.add_argument('--temperature', default=1.0, type=float, metavar='TEMPERATURE',
+                   help='Temperature of model 1 and model 2, shall be overriden if --scale-temperature')
 group.add_argument('--scale-temperature', action='store_true', default=False,
                    help='if true we apply optimal temperature scale to fit probabilities')
 group.add_argument('--logit-init', type=float, default=0.0, metavar='LOGIT',
@@ -489,7 +491,7 @@ def main():
         param.requires_grad = False
 
     # Create metamodel
-    model = meta_model.MetaModel(model1, model2, args.logit_init)
+    model = meta_model.MetaModel(model1, model2, args.temperature, args.logit_init)
 
     if args.head_init_scale is not None:
         with torch.no_grad():

temperature_scaling.py
@@ -11,10 +11,10 @@ class ModelWithTemperature(nn.Module):
         NB: Output of the neural network should be the classification logits,
             NOT the softmax (or log softmax)!
     """
-    def __init__(self, model):
+    def __init__(self, model, temperature=1.0):
         super(ModelWithTemperature, self).__init__()
         self.model = model
-        self.temperature = nn.Parameter(torch.ones(1))
+        self.temperature = nn.Parameter(torch.tensor([temperature]))
 
     def forward(self, input):
         logits = self.model(input)