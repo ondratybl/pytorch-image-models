d9854ce51f62463d219e9dcdac5730e0dbaaac53
ondrej <ondra@tybl.cz>
2024-04-14 13:55:01 +0200

    test

--- train.py
+++ train.py
@@ -858,7 +858,7 @@ def main():
     #if utils.is_primary(args) and args.log_wandb:
     if args.log_wandb:
         if has_wandb:
-            print(torch.cuda.current_device())
+            print('Init wandb on device ' + str(torch.cuda.current_device()))
             wandb.init(
                 project=args.experiment,
                 config=args,
