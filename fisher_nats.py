d58c5bceb545ff77cd9306f864cfcb21a3d01088
ondrej <ondra@tybl.cz>
2024-06-28 14:34:45 +0200

    fisher nats coefficient added

--- fisher_nats.py
+++ fisher_nats.py
@@ -1,13 +1,14 @@
 from xautodl.datasets.DownsampledImageNet import ImageNet16
 from nats_bench import create
 from xautodl.models import get_cell_based_tiny_net
-from fisher import get_eigenvalues, get_ntk_tenas_new, get_ntk_tenas_new_probs
+from fisher import cholesky_covariance, jacobian_batch_efficient, get_ntk_tenas_new, get_ntk_tenas_new_probs
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import torch
 import argparse
 import wandb
 import random
+import gc
 import numpy as np
 
 
@@ -61,110 +62,25 @@ def get_condition_number(matrix):
         return None
 
 
-def compute(model, index, seed, loader, num_fisher, num_tenas, device):
+def get_ntk(model, input):
 
-    # NTK
-    ntk = torch.zeros(120, 120, device=device)
-    for batch, data in enumerate(loader):
+    cholesky = cholesky_covariance(model(input))
+    jacobian = jacobian_batch_efficient(model, input)
 
-        if batch >= num_fisher:
-            break
+    ntk = torch.mean(torch.matmul(jacobian, torch.transpose(jacobian, dim0=1, dim1=2)), dim=0).detach()
+    A = torch.matmul(cholesky, jacobian).detach()
 
-        input, _ = data
-        input = input.to(device)
-        ntk = get_eigenvalues(model, input, model(input), ntk, batch)
+    del jacobian, cholesky
+    gc.collect()
+    torch.cuda.empty_cache()
 
-        if torch.isnan(ntk).sum().item() > 0:
-            print(f'NTK contains NaN. Output is stored.')
-            np.savetxt(f'output/output_{index}_{seed}_{batch}.csv', model(input).detach().cpu().numpy(), delimiter=',')
+    ntk_p = torch.mean(torch.matmul(A, torch.transpose(A, dim0=1, dim1=2)), dim=0).detach()
 
-        if batch % 100 == 0:
-            print(f'Index {index} seed {seed} batch {batch}')
-            if torch.cuda.is_available():
-                print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
-                print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
-                print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
-                torch.cuda.reset_peak_memory_stats()
+    del A
+    gc.collect()
+    torch.cuda.empty_cache()
 
-    ntk = ntk.float() / 1000000
-
-    print(f'Index {index} seed {seed} before TENAS')
-    if torch.cuda.is_available():
-        print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
-        print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
-        print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
-        torch.cuda.reset_peak_memory_stats()
-
-    # TENAS
-    output = []
-    for input, _ in list(loader)[:num_tenas]:
-        input = input.to(device)
-        output.append(model(input).squeeze(0))
-    output = torch.stack(output)
-    eig_tenas = get_ntk_tenas_new(model, output).detach()
-    eig_tenas_probs = get_ntk_tenas_new_probs(model, output).detach()
-
-    print(f'Index {index} seed {seed} after TENAS')
-    if torch.cuda.is_available():
-        print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
-        print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
-        print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
-        torch.cuda.reset_peak_memory_stats()
-
-    return {
-        'ntk_fro': torch.linalg.matrix_norm(ntk, ord='fro').item(),
-        'ntk_nuc': torch.linalg.matrix_norm(ntk, ord='nuc').item(),
-        'ntk_sing': torch.linalg.matrix_norm(ntk, ord=2).item(),
-        'ntk_cond': get_condition_number(ntk),
-
-        'tenas_max': eig_tenas.max().item(),
-        'tenas_sum': eig_tenas.sum().item(),
-        'tenas_sum2': torch.square(eig_tenas).sum().item(),
-        'tenas_std': eig_tenas.std().item(),
-        'tenas_cond': eig_tenas.max().item() / (eig_tenas.min().item()) if eig_tenas.min().item() > 0 else None,
-
-        'tenas_p_max': eig_tenas_probs.max().item(),
-        'tenas_p_sum': eig_tenas_probs.sum().item(),
-        'tenas_p_sum2': torch.square(eig_tenas_probs).sum().item(),
-        'tenas_p_std': eig_tenas_probs.std().item(),
-        'tenas_p_cond': eig_tenas_probs.max().item() / (
-            eig_tenas_probs.min().item()) if eig_tenas_probs.min().item() > 0 else -1,
-
-        'params_total': sum(p.numel() for n, p in model.named_parameters()),
-        'params_used': sum(p.numel() for n, p in model.named_parameters() if ('weight' in n and 'bn' not in n)),
-    }
-
-
-def compute_tenas_across_batches(model, index, seed, loader, num_tenas, device):
-
-    # TENAS
-    tenas_cond_list = []
-    tenas_cond_probs_list = []
-    for input, _ in list(loader)[:num_tenas]:
-
-        input = input.to(device)
-        output = model(input)
-
-        eig_tenas = get_ntk_tenas_new(model, output).detach()
-        eig_tenas_probs = get_ntk_tenas_new_probs(model, output).detach()
-
-        tenas_cond_list.append(eig_tenas.max().item() / (eig_tenas.min().item()) if eig_tenas.min().item() > 0 else None)
-        tenas_cond_probs_list.append(eig_tenas_probs.max().item() / (eig_tenas_probs.min().item()) if eig_tenas_probs.min().item() > 0 else None)
-
-    print(f'Index {index} seed {seed} after TENAS')
-    if torch.cuda.is_available():
-        print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
-        print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
-        print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
-        torch.cuda.reset_peak_memory_stats()
-
-    return {
-        'tenas_cond_list': tenas_cond_list,
-        'tenas_cond_probs_list': tenas_cond_probs_list,
-
-        'params_total': sum(p.numel() for n, p in model.named_parameters()),
-        'params_used': sum(p.numel() for n, p in model.named_parameters() if ('weight' in n and 'bn' not in n)),
-    }
+    return torch.linalg.eigvalsh(ntk), torch.linalg.eigvalsh(ntk_p)
 
 
 if __name__ == '__main__':
@@ -173,7 +89,7 @@ if __name__ == '__main__':
     # Add arguments
     parser.add_argument('--dataset', type=str, default='Data/ImageNet16-120', help='Dataset path.')
     parser.add_argument('--models', type=str, default='NATS-tss-v1_0-3ffb9-simple', help='Models path.')
-    parser.add_argument('--batch-size', type=int, default=1, help='Batch size (use > 1 only when computing TENAS across batches)')
+    parser.add_argument('--batch-size', type=int, default=32, help='Batch size.')
     parser.add_argument('--pretrained', action='store_true', help='Use pretrained weights.')
     parser.add_argument('--epochs_trained', type=str, default='200', help='Number of training epochs.')
     parser.add_argument('--n_model_min', type=int, default=0, help='Model from.')
@@ -186,10 +102,8 @@ if __name__ == '__main__':
                         help='Longer description of the run, like a -m commit message in git')
     parser.add_argument('--tags-wandb', default='default', type=str, metavar='NAME',
                         help='tags of the run')
-    parser.add_argument('--num-fisher', default=4096, type=int, metavar='NAME',
-                        help='Number of samples to be watched by FIM')
-    parser.add_argument('--num-tenas', default=32, type=int, metavar='NAME',
-                        help='Number of samples to be watched by TENAS')
+    parser.add_argument('--num-batches', default=10, type=int, metavar='NAME',
+                        help='Number of batches to be watched')
 
     # Parse the arguments
     args = parser.parse_args()
@@ -218,22 +132,57 @@ if __name__ == '__main__':
     for index in range(args.n_model_min, args.n_model_max):
         try:
             for seed in api.get_net_param(index, args.dataset_name, None, hp=args.epochs_trained).keys():
-                # get FIM and TENAS
+
                 model = get_model(api, args.dataset_name, index, seed, args.epochs_trained, args.pretrained).to(device)
 
-                if args.batch_size == 1:  # compute TENAS on one batch of size args.num_tenas and NTK on all
-                    info_ntk = compute(model, index, seed, loader, args.num_fisher, args.num_tenas, device)
-                else:  # compute TENAS on args.num_tenas batches of size args.batch_size only
-                    info_ntk = compute_tenas_across_batches(model, index, seed, loader, args.num_tenas, device)
                 # get train statistics
                 info_per = api.get_more_info(index, args.dataset_name, hp=args.epochs_trained, is_random=seed)
                 info_cost = api.get_cost_info(index, args.dataset_name, hp=args.epochs_trained)
 
-                # log
-                combined_dict = {'index': index, 'seed': seed, 'hp': args.epochs_trained}
-                combined_dict.update(info_ntk)
-                combined_dict.update(info_per)
-                combined_dict.update(info_cost)
-                wandb.log(combined_dict)
+                # get compute & log
+                for batch, (input, _) in enumerate(loader)[: args.num_batches]:
+
+                    input = input.to(device)
+
+                    # tenas
+                    eig_tenas = get_ntk_tenas_new(model, model(input)).detach()
+                    eig_tenas_p = get_ntk_tenas_new_probs(model, model(input)).detach()
+
+                    info_tenas = {
+                        'tenas_cond': eig_tenas.max() / eig_tenas.min() if eig_tenas.min() > 0 else None,
+                        'tenas_max': eig_tenas.max(),
+                        'tenas_coef': eig_tenas.std() / eig_tenas.mean() if eig_tenas.mean() > 0 else None,
+
+                        'tenas_p_cond': eig_tenas_p.max() / eig_tenas_p.min() if eig_tenas_p.min() > 0 else None,
+                        'tenas_p_max': eig_tenas_p.max(),
+                        'tenas_p_coef': eig_tenas_p.std() / eig_tenas_p.mean() if eig_tenas_p.mean() > 0 else None,
+                    }
+
+                    # ntk
+                    eig_ntk, eig_ntk_p = get_ntk(model, input)
+
+                    if torch.isnan(eig_ntk).sum().item() > 0 or torch.isnan(eig_ntk_p).sum().item():
+                        print(f'NTK contains NaN. Output is stored.')
+                        np.savetxt(f'output/output_{index}_{seed}_{batch}.csv', model(input).detach().cpu().numpy(),
+                                   delimiter=',')
+
+                    info_ntk = {
+                        'ntk_cond': eig_ntk.max() / eig_ntk.min() if eig_ntk.min() > 0 else None,
+                        'ntk_max': eig_ntk.max(),
+                        'ntk_coef': eig_ntk.std() / eig_ntk.mean() if eig_ntk.mean() > 0 else None,
+
+                        'ntk_p_cond': eig_ntk_p.max() / eig_ntk_p.min() if eig_ntk_p.min() > 0 else None,
+                        'ntk_p_max': eig_ntk_p.max(),
+                        'ntk_p_coef': eig_ntk_p.std() / eig_ntk_p.mean() if eig_ntk_p.mean() > 0 else None,
+                    }
+
+                    # log
+                    combined_dict = {'index': index, 'seed': seed, 'hp': args.epochs_trained, 'batch': batch}
+                    combined_dict.update(info_ntk)
+                    combined_dict.update(info_tenas)
+                    combined_dict.update(info_per)
+                    combined_dict.update(info_cost)
+                    wandb.log(combined_dict)
+
         except AssertionError as e:
             print(f"Model not found: {e}")
